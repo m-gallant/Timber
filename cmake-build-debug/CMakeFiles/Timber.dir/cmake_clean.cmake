0b9cca26acd3fc87a94b53f41571179672da4603
m-gallant <miss.mgallant@gmail.com>
2019-05-17 14:27:20 -0700

    initla project files, added font, graphics and sound files

---
+++ cmake-build-debug/CMakeFiles/Timber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Timber.dir/main.cpp.o"
+  "Timber.pdb"
+  "Timber"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Timber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
